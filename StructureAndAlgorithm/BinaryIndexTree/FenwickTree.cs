1497a66d9392e834fe9cad6a2670a944d078e39f
Vu Ton <vuton101982@gmail.com>
2019-08-31 12:35:24 +0700

    -fenwicktr ee

---
+++ StructureAndAlgorithm/BinaryIndexTree/FenwickTree.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StructureAndAlgorithm.BinaryIndexTree
+{
+    /***
+     * Implement Fenwick  sum tree  
+     **/
+    public class FenwickTree
+    {
+
+        private readonly int _size;
+
+        private readonly int[] _items;
+
+        public FenwickTree(int size)
+        {
+        }
+
+    }
+}
